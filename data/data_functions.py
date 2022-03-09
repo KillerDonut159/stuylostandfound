9b0e4f8d19ecd4760d6323518f4d92ec48014799
tomas-20 <tacuna20@stuy.edu>
2022-03-08 21:10:04 -0500

    further simplified data_functions.py and created the TABLE_HEADER and BLANK_TABLE_ROW constants

--- data/data_functions.py
+++ data/data_functions.py
@@ -6,17 +6,17 @@ FIELDS = ["number", "date", "description", "link"]
 def mapa(function, *lsts):
     return list(map(function, *lsts))
 
+def get_list_string(strings):
+    return f"({', '.join(strings)})"
+
 def add_type(field):
     return f"{field} TEXT"
 
 def get_question_mark(field):
     return "?"
 
-def get_list_string(strings):
-    return f"({', '.join(strings)})"
-
-def first(lst):
-    return lst[0]
+TABLE_HEADER = get_list_string(mapa(add_type, FIELDS))
+BLANK_TABLE_ROW = get_list_string(mapa(get_question_mark, FIELDS))
 
 def get_item(*values):
     return dict(zip(FIELDS, values))
@@ -26,13 +26,13 @@ def reset_data():
     db = connect("data.db")
     c = db.cursor()
     for table in TABLES:
-        c.execute(f"CREATE TABLE IF NOT EXISTS {table} {get_list_string(mapa(add_type, FIELDS))}")
+        c.execute(f"CREATE TABLE IF NOT EXISTS {table} {TABLE_HEADER}")
     db.commit()
 
 def add_item(table, item):
     db = connect("data.db")
     c = db.cursor()
-    c.execute(f"INSERT INTO {table} VALUES {get_list_string(mapa(get_question_mark, FIELDS))}", mapa(item.get, FIELDS))
+    c.execute(f"INSERT INTO {table} VALUES {BLANK_TABLE_ROW}", mapa(item.get, FIELDS))
     db.commit()
 
 def get_column(table, field):
@@ -41,10 +41,11 @@ def get_column(table, field):
     c.execute(f"SELECT {field} FROM {table}")
     return mapa(first, c.fetchall())
 
+def first(lst):
+    return lst[0]
+
 def get_items(table):
     def get_table_column(field):
         return get_column(table, field)
     columns = mapa(get_table_column, FIELDS)
-    def get_table_item(*values):
-        return dict(zip(FIELDS, values))
-    return mapa(get_table_item, *columns)
+    return mapa(get_item, *columns)
