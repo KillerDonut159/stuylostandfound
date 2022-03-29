ce4110338795fbdd9f82297016f429931d09d5ef
tomas-20 <tacuna20@stuy.edu>
2022-03-29 17:02:54 -0400

    added the get_items and update_found functions to data_functions.py

--- data2/data_functions.py
+++ data2/data_functions.py
@@ -1,4 +1,4 @@
-from sqlite3 import connect
+from sqlite3 import connect, Row
 
 
 def reset_data():
@@ -23,5 +23,31 @@ def add_item(category, date, description, link):
     db = connect("data.db")
     c = db.cursor()
     c.execute("INSERT INTO mesa VALUES (?, ?, ?, ?, ?, ?)", (get_next_id(), 0, category, date, description, link))
+    db.commit()
+    db.close()
+
+def row_to_dic(row):
+    dic = dict(row)
+    dic.pop("found")
+    dic.pop("category")
+    return dic
+
+def get_items(category):
+    db = connect("data.db")
+    c = db.cursor()
+    c.row_factory = Row
+    c.execute("SELECT * FROM mesa WHERE category = ? AND found = 0", (category,))
+    result = list(map(row_to_dic, c.fetchall()))
+    db.close()
+    return result
+
+def update_found(id, found):
+    db = connect("data.db")
+    c = db.cursor()
+    if found:
+        found = 1
+    else:
+        found = 0
+    c.execute("UPDATE mesa SET found = ? WHERE id = ?", (found, id))
     db.commit()
     db.close()
